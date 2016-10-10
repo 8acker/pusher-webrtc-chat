e0d9b335cb545a0067859e8b0918a0fd8b8f2f6b
tbouchnafa <thami.bouchnafa@1und1.de>
2016-10-10 09:57:39 +0200

    First import

---
+++ src/_servers/nodejs/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  app_id: "257494",
+  key: "456a579843f625827b9c",
+  secret: "0357f2ff871c7426c369",
+  cluster: 'eu',
+  encrypted: true
+};
--- src/_servers/php/config.example.php
+++
@@ -1,7 +0,0 @@
-<?php
-error_reporting(E_ALL);
-
-define("APP_ID", "PUSHER_APP_ID");
-define("APP_KEY", "PUSHER_APP_KEY");
-define("APP_SECRET", "PUSHER_APP_SECRET");
-?>
